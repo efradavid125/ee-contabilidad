cc8d8a7f897bf8edf226f6d6e394d59288822a42
efrain <efradavid125@gmail.com>
2017-11-16 11:34:27 -0400

    start of the app

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ee-contabilidad'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
